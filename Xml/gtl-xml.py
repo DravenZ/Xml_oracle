d1b3f1e4608ba4299c2411ae6e48de8c6e31e484
DravenZ <569555808@qq.com>
2017-09-22 17:36:40 +0800

    添加

--- Xml/gtl-xml.py
+++ Xml/gtl-xml.py
@@ -4,25 +4,30 @@ from xml.dom.minidom import parse
 import xml.dom.minidom
 
 # 使用minidom解析器打开 XML 文档
-DOMTree = xml.dom.minidom.parse("movies.xml")
+DOMTree = xml.dom.minidom.parse("JmeterReport.jtl")
 collection = DOMTree.documentElement
 if collection.hasAttribute("shelf"):
    print "Root element : %s" % collection.getAttribute("shelf")
 
 # 在集合中获取所有电影
-movies = collection.getElementsByTagName("movie")
-
+httpSamples = collection.getElementsByTagName("httpSample")
+httpSamples = list(httpSamples)
+print type(httpSamples)
+print "共有" + str(len(httpSamples)) + "个Http请求"
 # 打印每部电影的详细信息
-for movie in movies:
-   print "*****Movie*****"
-   if movie.hasAttribute("title"):
-      print "Title: %s" % movie.getAttribute("title")
+for httpSample in httpSamples:
+    if httpSample.hasAttribute("lb"):
+        print "Title: %s" % httpSample.getAttribute("lb")
 
-   type = movie.getElementsByTagName('type')[0]
-   print "Type: %s" % type.childNodes[0].data
-   format = movie.getElementsByTagName('format')[0]
-   print "Format: %s" % format.childNodes[0].data
-   rating = movie.getElementsByTagName('rating')[0]
-   print "Rating: %s" % rating.childNodes[0].data
-   description = movie.getElementsByTagName('description')[0]
-   print "Description: %s" % description.childNodes[0].data
+   # print "*****Movie*****"
+   # if movie.hasAttribute("title"):
+   #    print "Title: %s" % movie.getAttribute("title")
+
+   # type = movie.getElementsByTagName('type')[0]
+   # print "Type: %s" % type.childNodes[0].data
+   # format = movie.getElementsByTagName('format')[0]
+   # print "Format: %s" % format.childNodes[0].data
+   # rating = movie.getElementsByTagName('rating')[0]
+   # print "Rating: %s" % rating.childNodes[0].data
+   # description = movie.getElementsByTagName('description')[0]
+   # print "Description: %s" % description.childNodes[0].data
