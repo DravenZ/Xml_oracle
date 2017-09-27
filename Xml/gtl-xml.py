4e9d1e3abc07d040af92745d5094b0d94e4409c3
DravenZ <569555808@qq.com>
2017-09-27 15:20:09 +0800

    添加

--- Xml/gtl-xml.py
+++ Xml/gtl-xml.py
@@ -98,8 +98,6 @@ if __name__ == '__main__':
         print i
     for i in httpalllist:
         print i
-# if collection.hasAttribute("version"):
-#    print "version : %s" % collection.getAttribute("version")
 
 
 
@@ -109,16 +107,3 @@ if __name__ == '__main__':
 
 
 
-
-   # print "*****Movie*****"
-   # if movie.hasAttribute("title"):
-   #    print "Title: %s" % movie.getAttribute("title")
-
-   # type = movie.getElementsByTagName('type')[0]
-   # print "Type: %s" % type.childNodes[0].data
-   # format = movie.getElementsByTagName('format')[0]
-   # print "Format: %s" % format.childNodes[0].data
-   # rating = movie.getElementsByTagName('rating')[0]
-   # print "Rating: %s" % rating.childNodes[0].data
-   # description = movie.getElementsByTagName('description')[0]
-   # print "Description: %s" % description.childNodes[0].data
