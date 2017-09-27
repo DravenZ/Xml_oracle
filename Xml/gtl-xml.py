30311b9f3dd59904807f1709ee9ecd976f7aff36
DravenZ <569555808@qq.com>
2017-09-27 14:13:43 +0800

    添加

--- Xml/gtl-xml.py
+++ Xml/gtl-xml.py
@@ -12,25 +12,69 @@ dbname = 'test'
 sqldb = MysqlDb()
 conn = sqldb.conn(host,port,dbname,user,password)
 
-# 使用minidom解析器打开 XML 文档
-DOMTree = xml.dom.minidom.parse("JmeterReport.jtl")
-collection = DOMTree.documentElement
-if collection.hasAttribute("version"):
-   print "version : %s" % collection.getAttribute("version")
+class gtl():
+    filename = "JmeterReport.jtl"
+    def getCollection(self,filename):
+        # 使用minidom解析器打开 XML 文档
+        DOMTree = xml.dom.minidom.parse(filename)
+        collection = DOMTree.documentElement
+        return collection
 
-# 在集合中获取所有Http请求
-httpSamples = collection.getElementsByTagName("httpSample")
-httpSamples = list(httpSamples)
-# print type(httpSamples)
-print "共有" + str(len(httpSamples)) + "个Http请求"
-# 打印每个Http请求的详细信息
-for httpSample in httpSamples:
-    if httpSample.hasAttribute("lb"):
-        print "Http: %s" % httpSample.getAttribute("lb")
-        print httpSample.getAttribute("tn")
-        sqlinsert = "insert into USER(name,pwd) VALUES ('%s','%s')" % (httpSample.getAttribute("lb"),httpSample.getAttribute("tn"))
-        print sqlinsert
-        sqldb.sqlDML(sqlinsert,conn)
+    # 在集合中获取所有Http请求
+    def gethttplist(self,collection):
+        httpSamples = collection.getElementsByTagName("httpSample")
+        print type(httpSamples)
+        httpSamples = list(httpSamples)
+        # print "共有" + str(len(httpSamples)) + "个Http请求"
+        return httpSamples
+
+    def getCase_info(self,httpSamples):
+        # 打印每个Http请求的详细信息
+        for httpSample in httpSamples:
+            if httpSample.hasAttribute("lb"):
+                case_no = (httpSample.getAttribute("lb").encode("utf-8")).split('-')[0]
+                case_name = (httpSample.getAttribute("lb").encode("utf-8")).split('-')[1]
+                print "用例编号、用例名称: %s,%s" % (case_no,case_name)
+
+            if httpSample.hasAttribute("tn"):
+                creater = (httpSample.getAttribute("tn").encode("utf-8")).split('-')[0]
+                print "用例设计人员: %s" % creater
+                # sqlinsert = "insert into USER(name,pwd) VALUES ('%s','%s')" % (
+                # httpSample.getAttribute("lb"), httpSample.getAttribute("tn"))
+                # print sqlinsert
+                # sqldb.sqlDML(sqlinsert,conn)
+            # print type(httpSample)
+            if httpSample.hasAttribute("rc"):
+                print httpSample.getAttribute("rc")
+                if httpSample.getAttribute("rc") == '200':
+                    if httpSample.getElementsByTagName('assertionResult'):
+                        # print (list(httpSample.getElementsByTagName('assertionResult'))[0]).getElementsByTagName('name')[0]
+                        asserts = httpSample.getElementsByTagName('assertionResult')[0]
+                        print asserts.getElementsByTagName('name')[0].childNodes[0].nodeValue.strip()
+
+
+
+
+    # 获取用例总数
+    def get_run_case_count(self,httpSamples):
+        print "共有" + str(len(httpSamples)) + "个Http请求"
+        return len(httpSamples)
+
+    # def get_succ_count(self):
+
+
+if __name__ == '__main__':
+    g = gtl()
+    collection = g.getCollection(g.filename)
+    httplist = g.gethttplist(collection)
+    g.getCase_info(httplist)
+
+# if collection.hasAttribute("version"):
+#    print "version : %s" % collection.getAttribute("version")
+
+
+
+
 
 
 
