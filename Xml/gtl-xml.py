5188f8b20c0907fc57560da5134c9473734522fe
DravenZ <569555808@qq.com>
2017-09-28 16:44:13 +0800

    添加

--- Xml/gtl-xml.py
+++ Xml/gtl-xml.py
@@ -17,21 +17,20 @@ conn = sqldb.conn(host,port,dbname,user,password)
 
 class gtl():
     filename = "JmeterReport.jtl"
-    def getCollection(self,filename):
+    def getCollection(self, filename):
         # 使用minidom解析器打开 XML 文档
         DOMTree = xml.dom.minidom.parse(filename)
         collection = DOMTree.documentElement
         return collection
 
     # 在集合中获取所有Http请求
-    def gethttplist(self,collection):
+    def gethttplist(self, collection):
         httpSamples = collection.getElementsByTagName("httpSample")
-        print type(httpSamples)
         httpSamples = list(httpSamples)
-        print "共有" + str(len(httpSamples)) + "个Http请求"
+        # print "共有" + str(len(httpSamples)) + "个Http请求"
         return httpSamples
 
-    def getCase_info(self,httpSamples):
+    def getCase_info(self, httpSamples):
         # 打印每个Http请求的详细信息
         fail_count = 0
         httpalllist = []
@@ -45,27 +44,27 @@ class gtl():
                 case_name = (httpSample.getAttribute("lb").encode("utf-8")).split('-')[1]
                 httpdata['case_no'] = case_no
                 httpdata['case_name'] = case_name
-                print "用例编号、用例名称: %s,%s" % (case_no,case_name)
+                # print "用例编号、用例名称: %s,%s" % (case_no,case_name)
 
             if httpSample.hasAttribute("tn"):
                 creater = (httpSample.getAttribute("tn").encode("utf-8")).split('-')[0]
                 httpdata['creater'] = creater
-                print "用例设计人员: %s" % creater
+                # print "用例设计人员: %s" % creater
 
             if httpSample.hasAttribute("rc"):
-                print httpSample.getAttribute("rc")
+                # print httpSample.getAttribute("rc")
                 if httpSample.getElementsByTagName('assertionResult'):
                     asserts = httpSample.getElementsByTagName('assertionResult')[0]
-                    failure =  asserts.getElementsByTagName('failure')[0].childNodes[0].nodeValue.strip()
+                    failure = asserts.getElementsByTagName('failure')[0].childNodes[0].nodeValue.strip()
                     error = asserts.getElementsByTagName('error')[0].childNodes[0].nodeValue.strip()
                     if error != 'false' or failure != 'false' or httpSample.getAttribute("rc") != '200':
                         fail_cause = asserts.getElementsByTagName('failureMessage')[0].childNodes[0].nodeValue.strip()
+                        # print fail_cause
                         timestamp = httpSample.hasAttribute("ts")
                         # 转换成localtime
                         time_local = time.localtime(timestamp)
                         # 转换成新的时间格式(2016-05-05 20:28:54)
                         run_date = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
-                        print fail_cause
                         fail_count = fail_count + 1
                         httpfaildata['case_no'] = case_no
                         httpfaildata['case_name'] = case_name
@@ -74,8 +73,7 @@ class gtl():
                         httpfaildata['run_date'] = run_date
                         httpfaillist.append(httpfaildata)
             httpalllist.append(httpdata)
-        #print "失败的用例条数：",fail_count
-        return fail_count,httpalllist,httpfaillist
+        return fail_count, httpalllist, httpfaillist
 
 
 
@@ -93,11 +91,16 @@ if __name__ == '__main__':
     collection = g.getCollection(g.filename)
     httplist = g.gethttplist(collection)
     failcount,httpalllist,httpfaillist = g.getCase_info(httplist)
-    print "失败的用例条数：",failcount
+    allcount = g.get_run_case_count(httplist)
+    print "所有的用例条数：", allcount
+    print "失败的用例条数：", failcount
+    print "成功的用例条数：", allcount - failcount
     for i in httpfaillist:
-        print i
+        for key, value in i.items():
+            print key, value
     for i in httpalllist:
-        print i
+        for key, value in i.items():
+            print key, value
 
 
 
