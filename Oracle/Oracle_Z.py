12ba1feba05f7c55ae000b37b9ab7d5d407f1230
DravenZ <569555808@qq.com>
2017-10-10 14:49:18 +0800

    修改函数参数，添加close函数

--- Oracle/Oracle_Z.py
+++ Oracle/Oracle_Z.py
@@ -8,26 +8,25 @@ class Oracle:
     """
     def __init__(self, db_name, user, password):
         """
-                功能：连接oracle数据库
-                参数：user,password,dbname(用户名、密码 、数据库)
-                返回：__init__无返回
-                作者：zhangpengfei
+            功能：连接oracle数据库
+            参数：user,password,dbname(用户名、密码 、数据库)
+            返回：__init__无返回
+            作者：zhangpengfei
             """
         try:
-            connect = cx_Oracle.connect(user, password, db_name)
-            self.connect = connect
+            self.connect = cx_Oracle.connect(user, password, db_name)
         except Exception as e:
             print("Connect failed",e)
 
-    def __sqlsearch__(self, sql, db):
+    def __sqlsearch__(self, sql):
         """
             功能：查询
-            参数：sql,db(sql语句,连接db)
+            参数：sql(sql语句)
             返回：成功返回True和查询结果（list中套的元组），失败返回False和异常信息
             作者：zhangpengfei
             """
         try:
-            cur = db.cursor()
+            cur = self.connect.cursor()
             x = cur.execute(sql)
             results = x.fetchall()
             cur.close()
@@ -36,25 +35,37 @@ class Oracle:
             print("search failed", e)
             return False, e
 
-    def __sqloperation__(self, sql, db, params=None):
+    def __sqloperation__(self, sql, params=None):
         """
-               功能：查询
-               参数：sql,db，params(sql语句,连接db,params参数是选填参数，若不写该参数，sql语句应该是完整无误的)
-               返回：成功返回True和影响结果，失败返回False和异常信息
-               作者：zhangpengfei
-           """
+            功能：查询
+            参数：sql,db，params(sql语句,连接db,params参数是选填参数，若不写该参数，sql语句应该是完整无误的)
+            返回：成功返回True和影响结果，失败返回False和异常信息
+            作者：zhangpengfei
+            """
         try:
-            cr = db.cursor()
+            cr = self.connect.cursor()
             if params != None:
                 cr.executemany(sql, params)
             else:
                 cr.execute(sql)
             rowcount = cr.rowcount
             cr.close()
-            db.commit()
+            self.connect.commit()
             return True, rowcount
         except Exception as e:
             print("sqloperation failed", e)
+            return False, e
+    def close(self):
+        """
+            功能：关闭连接
+            参数：无
+            返回：成功返回True和conn，失败返回False和异常信息
+            作者：zhangpengfei
+            """
+        try:
+            conn = self.connect.close()
+            return True, conn
+        except Exception as e:
             return False, e
 
 if __name__ == '__main__':
@@ -79,10 +90,10 @@ if __name__ == '__main__':
     # connectresult, conn = O.conn(dbname, user, password)
 
     # 单条插入
-    opresult, mun = O.__sqloperation__(sqlinsert, O.connect)
+    opresult, mun = O.__sqloperation__(sqlinsert)
     print mun
     # 查询
-    searchresult, re = O.__sqlsearch__(sqlselect, O.connect)
+    searchresult, re = O.__sqlsearch__(sqlselect)
     print("---插入后查询---")
     # print type(re)
     for i in re:
@@ -90,43 +101,44 @@ if __name__ == '__main__':
         print i
 
     # 单条更新
-    opresult, mun = O.__sqloperation__(sqlupdate, O.connect)
+    opresult, mun = O.__sqloperation__(sqlupdate)
     print mun
-    searchresult, re = O.__sqlsearch__(sqlselect, O.connect)
+    searchresult, re = O.__sqlsearch__(sqlselect)
     print("---更新后查询---")
     for i in re:
         print i
 
     # 单条删除
-    opresult, mun = O.__sqloperation__(sqldelete, O.connect)
+    opresult, mun = O.__sqloperation__(sqldelete)
     print mun
-    searchresult, re = O.__sqlsearch__(sqlselect, O.connect)
+    searchresult, re = O.__sqlsearch__(sqlselect)
     print("---删除后查询---")
     for i in re:
         print i
 
     # 多条插入
-    opresult, mun =O.__sqloperation__(sqlinsertdml2, O.connect,  insertparam)
+    opresult, mun =O.__sqloperation__(sqlinsertdml2,  insertparam)
     print mun
-    searchresult, re = O.__sqlsearch__(sqlselect, O.connect)
+    searchresult, re = O.__sqlsearch__(sqlselect)
     print("---DML2插入后查询---")
     for i in re:
         print i
 
     # 多条更新
-    opresult, mun = O.__sqloperation__(sqlupdatedml2, O.connect, updateparam)
+    opresult, mun = O.__sqloperation__(sqlupdatedml2, updateparam)
     print mun
-    searchresult, re = O.__sqlsearch__(sqlselect, O.connect)
+    searchresult, re = O.__sqlsearch__(sqlselect)
     print("---DML2更新后查询---")
     for i in re:
         print i
 
     # 多条删除
-    opresult, mun = O.__sqloperation__(sqldeletedml2, O.connect, deleteparam)
+    opresult, mun = O.__sqloperation__(sqldeletedml2, deleteparam)
     print mun
-    searchresult, re = O.__sqlsearch__(sqlselect, O.connect)
+    searchresult, re = O.__sqlsearch__(sqlselect)
     print("---DML2删除后查询---")
     for i in re:
         print i
 
-    O.connect.close()
+    result, conn = O.close()
+    print result, conn
